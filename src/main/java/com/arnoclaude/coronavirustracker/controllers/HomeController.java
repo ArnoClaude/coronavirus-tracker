4a78458e5d94a80b8a3e2f4ce2e17c8524660b22
Arno Claude <arno.claude@googlemail.com>
2020-03-23 21:29:31 +0100

    First version. Result of the youtube tutorial.

---
+++ src/main/java/com/arnoclaude/coronavirustracker/controllers/HomeController.java
@@ -0,0 +1,30 @@
+package com.arnoclaude.coronavirustracker.controllers;
+
+import com.arnoclaude.coronavirustracker.models.LocationStats;
+import com.arnoclaude.coronavirustracker.services.CoronaVirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    private CoronaVirusDataService coronaVirusDataService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        //those attributes get called in the html file
+        List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+        int totalCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalCases);
+        model.addAttribute("totalNewCases", totalNewCases);
+
+        return "home";
+    }
+}
